cb485f77b8135149488d75f0889b5f72cf234360
Sagnac <83491030+Sagnac@users.noreply.github.com>
2022-08-21 00:03:35 +0000

    v0.20.5

--- streamsave.lua
+++ streamsave.lua
@@ -1,8 +1,8 @@
 --[[ 
 
 streamsave.lua
-Version 0.20.4
-2022-7-22
+Version 0.20.5
+2022-8-21
 https://github.com/Sagnac/streamsave
 
 mpv script aimed at saving live streams and clipping online videos without encoding.
